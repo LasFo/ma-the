750dab87cb64f6b886ae982beb26efc635774371
Lasse Folger <lfolger@localhost.localdomain>
2017-03-23 11:37:17 +0100

    added most important test: dining philosophers

---
+++ tests/nonterminatingTests/Philosophers.hs
@@ -0,0 +1,43 @@
+
+import STM
+import Control.Concurrent
+
+{-The most basic test case for synchronization tools:
+ - The dining Philosophers-}
+
+philosophers = 5
+
+type Stick = TVar Bool
+
+philosopher :: Int -> Stick -> Stick -> IO ()
+philosopher n sl sr = do
+  putStrLn $ "Phil" ++ show n ++ ": thinking..."
+  randomDelay
+  atomically $ takeStick sl >> takeStick sr
+  putStrLn $ "Phil" ++ show n ++ ": eating..."
+  atomically $ putStick sl >> putStick sr
+  philosopher n sl sr
+
+randomDelay :: IO ()
+randomDelay = do
+  waitTime <- getStdRandom (randomR (1,1000000))
+  threadDelay waitTime
+
+takeStick :: Stick -> STM ()
+takeStick s = do
+  avail <- readTVar s
+  if avail
+     then writeTVar s False
+     else retry
+
+putStick :: Stick -> STM ()
+putStick = flip writeTVar True
+
+main = do 
+  sticks <- atomically $ seqeunce $ replicate philosophers (newTVar True)
+  mapM_ forkIO $ makePhils philosophers sticks
+  getLine
+
+makePhils :: Int -> [Stick] -> [IO ()]
+makePhils 0 _        = []
+makePhils n (x:y:xs) = philosopher n x y : makePhils (n-1) (y:xs ++ [x])
